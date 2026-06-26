ff0dbb11e34b8b909953ec19088bb76efc96b94f
Tanu <tanuvish3565@gmail.com>
2026-06-26 23:13:39 +0530

    Implement user synchronization and enhance API integration with axios

---
+++ frontend/src/hooks/useUserSync.js
@@ -0,0 +1,22 @@
+import { useAuth, useUser } from "@clerk/react"
+import { useMutation } from "@tanstack/react-query"
+import { useEffect } from "react"
+import { syncUser } from "../lib/api.js"
+const useUserSync = () => {
+    const { isSignedIn } = useAuth()
+    const { user } = useUser()
+    const { mutate: syncUserMutation, isPending, isSuccess } = useMutation({ mutationFn:syncUser })
+
+    useEffect(() => {
+        if(isSignedIn && user && !isPending && !isSuccess){
+            syncUserMutation({
+                email: user.primaryEmailAddress.emailAddress,
+                name: user.fullName || user.firstName,
+                imageUrl: user.imageUrl
+            })
+        }
+    },[isSignedIn,user,syncUserMutation,isPending,isSuccess])
+  return {}
+}
+
+export default useUserSync
