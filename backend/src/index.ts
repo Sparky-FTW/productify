a21eb089d847869648596260a77e2071733cb96b
sparky-ftw <mandeepvish123@gmail.com>
2026-06-29 16:52:20 +0530

    Add backend package.json and update start script for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors"
 import userRoutes from "./routes/userRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import commentRoutes from "./routes/commentRoutes.js"
+import path from "path"
 
 
 const app = express()
@@ -15,19 +16,29 @@ app.use(clerkMiddleware())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req,res) => { 
-    res.json({
-        message: "Welcome to Productify API - Powered by PostgreSQL, Drizzzle ORM & Clerk Auth",
-        endpoints: {
-            users: "/api/users",
-            products: "/api/products",
-            comments: "/api/comments"
-        }
-    })
-})
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
 
 app.use("/api/users", userRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/comments", commentRoutes)
+
+if(ENV.NODE_ENV === "production"){
+    const __dirname = path.resolve()
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/{*any}", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT))
